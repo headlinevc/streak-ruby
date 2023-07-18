9c84b884d5d1516f89f1dfe882204265e9817adb
Bruno Melo <bruno.raphael@gmail.com>
2023-07-18 19:14:27 -0300

    Fix encoding when searching with special chars

--- lib/streak/search.rb
+++ lib/streak/search.rb
@@ -1,7 +1,8 @@
 module Streak
   class Search < StreakObject
     def self.query(s_query)
-      res = Streak.request(:get, "/v1/search", {query: s_query})
+      query = s_query.is_a?(String) ? {query: s_query} : s_query
+      res = Streak.request(:get, "/v1/search", query)
       convert_to_streak_object(res, Search)
     end
   end
